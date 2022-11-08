c645484a852b5255408a21076c908099c1c1002b
mingaleev.d92@mail.ru <tatarstan1204BTc)>
2022-11-08 12:06:06 +0300

    create AppModule и проверка соединения

--- app/src/main/java/com/example/pokedex/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/activities/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.pokedex.ui.activities
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.pokedex.R
 
 class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
+      setTheme(R.style.splashTheme)
       setContentView(R.layout.activity_main)
+
+      if(!isNetworkConnected()){
+         Toast.makeText(this, getString(R.string.check_internet_connections), Toast.LENGTH_SHORT).show()
+      }
+   }
+
+   private fun isNetworkConnected(): Boolean {
+      val connectivityManager =
+         getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+      val capabilities =
+         connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+
+      if (capabilities != null) {
+         if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(
+               (NetworkCapabilities.TRANSPORT_CELLULAR)
+            )
+         ) {
+            return true
+         }
+      }
+      return false
    }
 }
